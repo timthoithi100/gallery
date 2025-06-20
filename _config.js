753915b4bec529cda56dcdd047633c84584ff6be
Tim Thoithi <timthoithi100@gmail.com>
2025-06-20 15:59:26 +0300

    Connection string changes

--- _config.js
+++ _config.js
@@ -1,8 +1,8 @@
 var config = {}
 
 config.mongoURI = {
-    production: 'mongodb+srv://moringa:12345@gallery.gw7i9ol.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
-    development: 'mongodb+srv://moringa:12345@gallery.gw7i9ol.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
-    test: 'mongodb+srv://moringa:12345@gallery.gw7i9ol.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
+    production: 'mongodb+srv://moringa:12345@gallery.n1lq7ce.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
+    development: 'mongodb+srv://moringa:12345@gallery.n1lq7ce.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
+    test: 'mongodb+srv://moringa:12345@gallery.n1lq7ce.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
 }
 module.exports = config;
